dd0d8dc8b31e6dbae141b55ea6114b5fdce89eed
kimkunwoo99 <kimkunwoo99@gmail.com>
2025-04-30 09:58:29 +0900

    BookMarket의 5장 카테고리와 출판사명이 필터링된 검색목록

--- src/main/java/kr/ac/kopo/konumon/bookmarket/service/BookServiceImpl.java
+++ src/main/java/kr/ac/kopo/konumon/bookmarket/service/BookServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Service
 public class BookServiceImpl implements BookService {
@@ -27,5 +29,9 @@ public class BookServiceImpl implements BookService {
     public List<Book> getBookListByCategory(String category) {
         List<Book> booksByCategory = bookRepository.getBookListByCategory(category);
         return booksByCategory;
+    }
+    public Set<Book> getBookListByFilter(Map<String, List<String>> filter) {
+        Set<Book> booksByFilter = bookRepository.getBookListByFilter(filter);
+        return booksByFilter;
     }
 }
