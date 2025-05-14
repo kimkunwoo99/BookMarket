beb7aecfedcf5da2572e61536fc0e4614c70e955
kimkunwoo99 <kimkunwoo99@gmail.com>
2025-05-14 11:42:13 +0900

    BookMarket의 9장 유효성 검사

--- src/main/java/kr/ac/kopo/konumon/bookmarket/domain/Book.java
+++ src/main/java/kr/ac/kopo/konumon/bookmarket/domain/Book.java
@@ -1,13 +1,6 @@
 package kr.ac.kopo.konumon.bookmarket.domain;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.math.BigDecimal;
-
+import jakarta.validation.constraints.*;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,8 +11,13 @@ import java.math.BigDecimal;
 //@Setter
 //@NoArgsConstructor
 public class Book {
+    @Pattern(regexp = "isbn[0-9]+")
     private String bookId; //도서번호
+    @Size(min = 4, max = 50)
     private String name; //도서명
+    @Min(0)
+    @Digits(integer = 8, fraction = 2)
+    @NotNull
     private BigDecimal unitPrice; //단가
     private String author; //저자
     private String description; //도서설명
